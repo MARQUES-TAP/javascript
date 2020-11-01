bf6da1199cc27b45506f0556ed8c57ffb6f47111
marques-tap <piedmotor@gmail.com>
2020-10-31 21:35:28 -0300

    correção da aula 1

--- variaveis/index.js
+++ variaveis/index.js
@@ -16,6 +16,7 @@ console.log(resultado)
 */
 // array uma coleção de dados que pode agregar qualquer tipo
 //indice o ponteiro de uma posição, que sempre começa em zero
+
 /*const sabores = ["calabres", "nutella", "queijo"] //coleção de dados array  simples
 const tamanhos = ["70gr", "120gr", "170gr"]
 const valores = [7, 5, 3]
@@ -33,7 +34,7 @@ console.log(valores[0])
 */
 
 // objetos (parece com array , porém o indice é uma propriedade)
-const cliente = {
+/*const cliente = {
         nome: "fred",
         telefone: "xxxxxx",
         nota: 3
@@ -53,13 +54,82 @@ const pedido = {
     itensDoPedido: itens
 
 }
+console.log(pedido.itensDoPedido[0].sabor) *
+    /
 
-//console.log(pedido.itensDoPedido[0].sabor)
+*/
 //exercicio 1
 // criar um objeto com a propriedade cliente que tenha os dados ( nome , telefone .email)
-// intens do pedido , será um array de itens ( sabor , codigo,preço e tamanho)
-// propriedade forma de pagamento ( objeto que tem o nome e bandeira (ex: debt , crédt master ou visa))
-// tudo dentro do pedido
+const cliente = {
+        nome: "fred",
+        telefone: "xxxxxx",
+        email: "meuemail"
+    }
+    // intens do pedido , será um array de itens ( sabor , codigo,preço e tamanho)
+const itensDoPedido = [{
+            sabor: "calabresa",
+            codigo: "cd001",
+            tamanho: "70gr",
+            preco: 3
+        },
+        {
+            sabor: "queijo",
+            codigo: "cd002",
+            tamanho: "1200gr",
+            preco: 5
+        }
+    ]
+    //console.log(itensDoPedido)
+    //propriedade forma de pagamento ( objeto que tem o nome e bandeira (ex: debt , crédt master ou visa))
+    // tudo dentro do pedido
+const formaDePagamento1 = {
+    tipo: "cartao",
+    bandeira: "mastercard",
+    emissor: "banco do brasil"
+}
+const formaDePagamento2 = {
+    tipo: "cartao",
+    bandeira: "visa",
+    emissor: "santander"
+}
+const formaDePagamento3 = {
+    tipo: "dinheiro",
+    bandeira: "",
+    emissor: ""
+}
+const formasDePagamentoAceitas = [formaDePagamento1, formaDePagamento2, formaDePagamento3]
+const codigoDoPedido = "0001"
+const pedido1 = {
+    numeroPedido: codigoDoPedido,
+    horaDoPedido: "23:50",
+    cliente: cliente,
+    itens: itensDoPedido,
+    formaDePagamento: formasDePagamentoAceitas[1].tipo
+}
+
+const pedido2 = {
+    numeroPedido: "0002",
+    horaDoPedido: "22:50",
+    cliente: cliente,
+    itens: [itensDoPedido[1]],
+    formaDePagamento: formasDePagamentoAceitas[1].tipo
+}
+console.log(pedido1)
+
+const finalizar = {
+    clientes: cliente,
+}
 
 // exercicio 2
-// objeto chamado balanço do dia ( com data - dia semana - pedido ( array de pedido))
+// objeto chamado balanço do dia ( com data - dia semana - pedido  array de pedido)
+
+const balanco = {
+    data: "23/10/2020",
+    dia: "sabado",
+    pedidos: [
+        pedido1.numeroPedido,
+        pedido2.numeroPedido
+    ]
+
+}
+console.log(balanco)
